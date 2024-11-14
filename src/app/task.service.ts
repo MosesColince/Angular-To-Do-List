926a195627fb3a36653f1022679fb61dd2be99bb
Sithomola Mushe <123087447+MosesColince@users.noreply.github.com>
2024-11-14 03:30:53 +0200

    added enviroments files

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 interface Task {
   id: number;
@@ -15,7 +16,7 @@ interface Task {
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'https://sithomola-mushe-todo-list.netlify.app/';
+  private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
